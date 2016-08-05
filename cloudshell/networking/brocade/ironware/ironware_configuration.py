e675bb17219392f991488e0bce47b9fb2f12f9ea
Alex <alexey.b@qualisystems.com>
2016-08-05 16:21:24 +0300

    Brocade IronWare initial commit

---
+++ cloudshell/networking/brocade/ironware/ironware_configuration.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+from collections import OrderedDict
+
+# from cloudshell.cli.session.session_creator import SessionCreator
+# from cloudshell.cli.session.session_proxy import ReturnToPoolProxy
+# from cloudshell.cli.session.telnet_session import TelnetSession
+# from cloudshell.configuration.cloudshell_cli_configuration import CONNECTION_TYPE_TELNET
+from cloudshell.shell.core.context_utils import get_resource_address, get_attribute_by_name_wrapper
+from cloudshell.shell.core.dependency_injection.context_based_logger import get_logger_with_thread_id
+from cloudshell.snmp.quali_snmp_cached import QualiSnmpCached
+from cloudshell.shell.core.context_utils import get_decrypted_password_by_attribute_name_wrapper
+
+
+DEFAULT_PROMPT = r'[>%#]\s*$|[>%#]\s*\n'
+ENABLE_PROMPT = r'#\s*$'
+CONFIG_MODE_PROMPT = r'.*#\s*$'
+ENTER_CONFIG_MODE_PROMPT_COMMAND = 'configure terminal'
+EXIT_CONFIG_MODE_PROMPT_COMMAND = 'exit'
+SUPPORTED_OS = ["IronWare"]
+
+
+def enter_enable_mode(session):
+    session.hardware_expect('enable', re_string=DEFAULT_PROMPT + '|' + ENABLE_PROMPT,
+                            expect_map={'[Pp]assword': lambda session: session.send_line(
+                                # get_attribute_by_name_wrapper('Enable Password')())})
+                                get_decrypted_password_by_attribute_name_wrapper('Enable Password')())})
+    result = session.hardware_expect('', re_string=r"{}|{}".format(DEFAULT_PROMPT, ENABLE_PROMPT))
+    if not re.search(ENABLE_PROMPT, result):
+        raise Exception('enter_enable_mode', 'Enable password is incorrect')
+
+    return result
+
+
+def send_default_actions(session):
+    """Send default commands to configure/clear session outputs
+
+    :return:
+    """
+
+    out = ''
+    out += enter_enable_mode(session=session)
+    out += session.hardware_expect('skip-page-display', ENABLE_PROMPT)
+    out += session.hardware_expect(ENTER_CONFIG_MODE_PROMPT_COMMAND, CONFIG_MODE_PROMPT)
+    out += session.hardware_expect('no logging console', CONFIG_MODE_PROMPT)
+    out += session.hardware_expect('exit', r"{}|{}".format(DEFAULT_PROMPT, ENABLE_PROMPT))
+    return out
+
+
+def create_snmp_handler():
+    kwargs = {}
+    for key, value in QUALISNMP_INIT_PARAMS.iteritems():
+        if callable(value):
+            kwargs[key] = value()
+        else:
+            kwargs[key] = value
+    return QualiSnmpCached(**kwargs)
+
+DEFAULT_ACTIONS = send_default_actions
+
+QUALISNMP_INIT_PARAMS = {'ip': get_resource_address,
+                         'snmp_version': get_attribute_by_name_wrapper('SNMP Version'),
+                         'snmp_user': get_attribute_by_name_wrapper('SNMP V3 User'),
+                         'snmp_password': get_attribute_by_name_wrapper('SNMP V3 Password'),
+                         'snmp_community': get_attribute_by_name_wrapper('SNMP Read Community'),
+                         'snmp_private_key': get_attribute_by_name_wrapper('SNMP V3 Private Key')}
+
+
+SNMP_HANDLER_FACTORY = create_snmp_handler
+
+GET_LOGGER_FUNCTION = get_logger_with_thread_id
+
+ERROR_MAP = OrderedDict(
+    {r'[Ee]rror\s+saving\s+configuration': 'Save configuration error',
+     r'syntax\s+error': 'Syntax error',
+     r'[Uu]nknown\s+command': 'Uncnown command',
+     r'[Ee]rror\s+.+': 'Error'})
+
+
+# """Definition for Telnet session"""
+# CONNECTION_MAP = OrderedDict()
+# telnet_session = SessionCreator(TelnetSession)
+# telnet_session.proxy = ReturnToPoolProxy
+# telnet_session.kwargs = {'username': get_attribute_by_name_wrapper('User'),
+#                          'password': get_attribute_by_name_wrapper('Password'),
+#                          'host': get_resource_address}
+# CONNECTION_MAP[CONNECTION_TYPE_TELNET] = telnet_session
---
+++ cloudshell/networking/brocade/ironware/ironware_resource_driver.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import cloudshell.networking.brocade.ironware.ironware_configuration as driver_config
+
+from cloudshell.networking.brocade.autoload.brocade_snmp_autoload import BrocadeSnmpAutoload
+from cloudshell.networking.brocade.ironware.handler.brocade_ironware_operations import BrocadeIronWareOperations
+from cloudshell.networking.brocade.ironware.ironware_driver_bootstrap import IronWareDriverBootstrap
+
+from cloudshell.networking.networking_resource_driver_interface import NetworkingResourceDriverInterface
+# from cloudshell.networking.brocade.ironware.handler.brocade_ironware_connectivity_operations import \
+#     BrocadeIronwareConnectivityOperations
+
+from cloudshell.shell.core.context_utils import ContextFromArgsMeta
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+
+
+class IronWareResourceDriver(ResourceDriverInterface, NetworkingResourceDriverInterface):
+    __metaclass__ = ContextFromArgsMeta
+
+    def __init__(self, config=None):
+        bootstrap = IronWareDriverBootstrap()
+        bootstrap.add_config(driver_config)
+        if config:
+            bootstrap.add_config(config)
+        bootstrap.initialize()
+
+    # @property
+    # def connectivity_operations(self):
+    #     return BrocadeIronwareConnectivityOperations()
+
+    @property
+    def operations(self):
+        return BrocadeIronWareOperations()
+
+    @property
+    def autoload(self):
+        return BrocadeSnmpAutoload()
+
+    def initialize(self, context):
+        pass
+
+    def cleanup(self):
+        pass
+
+    # def ApplyConnectivityChanges(self, context, request):
+    #     return self.connectivity_operations.apply_connectivity_changes(request)
+
+    def shutdown(self, context):
+        self.operations.shutdown()
+
+    def get_inventory(self, context):
+        return self.autoload.discover()
+
+    def save(self, context, folder_path, configuration_type):
+        self.operations.save_configuration(folder_path, configuration_type)
+
+    def send_custom_config_command(self, context, command):
+        return self.operations.send_config_command(command)
+
+    def send_custom_command(self, context, command):
+        return self.operations.send_command(command)
+
+    def update_firmware(self, context, remote_host, file_path):
+        return self.operations.update_firmware(remote_host, file_path)
+
+    def restore(self, context, path, config_type, restore_method):
+        return self.operations.restore_configuration(path, config_type, restore_method)
