769aa3583949fca9cbce670458c00ad4fcc4be15
alexquali <alexey.b@quali.com>
2017-03-02 17:35:17 +0200

    Fix the issue when enable mode asked login and password, not only password

--- cloudshell/networking/brocade/netiron/netiron_configuration.py
+++ cloudshell/networking/brocade/netiron/netiron_configuration.py
@@ -3,9 +3,9 @@
 
 import re
 
-from cloudshell.shell.core.context_utils import get_decrypted_password_by_attribute_name_wrapper
+from cloudshell.shell.core.context_utils import get_attribute_by_name_wrapper, \
+    get_decrypted_password_by_attribute_name_wrapper
 from cloudshell.shell.core.dependency_injection.context_based_logger import get_logger_with_thread_id
-
 
 DEFAULT_PROMPT = r'[>%#]\s*$|[>%#]\s*\n'
 ENABLE_PROMPT = r'#\s*$'
@@ -18,8 +18,10 @@ HE_MAX_LOOP_RETRIES = 0
 
 def enter_enable_mode(session):
     session.hardware_expect('enable', re_string=DEFAULT_PROMPT + '|' + ENABLE_PROMPT,
-                            expect_map={'[Pp]assword': lambda session: session.send_line(
-                                get_decrypted_password_by_attribute_name_wrapper('Enable Password')())})
+                            expect_map={'[Ll]ogin|[Uu]ser\s*([Nn]ame)*': lambda session: session.send_line(
+                                get_attribute_by_name_wrapper('User')()),
+                                        '[Pp]assword': lambda session: session.send_line(
+                                            get_decrypted_password_by_attribute_name_wrapper('Enable Password')())})
     result = session.hardware_expect('', re_string=r"{}|{}".format(DEFAULT_PROMPT, ENABLE_PROMPT))
     if not re.search(ENABLE_PROMPT, result):
         raise Exception('enter_enable_mode', 'Enable password is incorrect')
@@ -40,6 +42,7 @@ def send_default_actions(session):
     out += session.hardware_expect('no logging console', CONFIG_MODE_PROMPT)
     out += session.hardware_expect('exit', ENABLE_PROMPT)
     return out
+
 
 DEFAULT_ACTIONS = send_default_actions
 GET_LOGGER_FUNCTION = get_logger_with_thread_id
